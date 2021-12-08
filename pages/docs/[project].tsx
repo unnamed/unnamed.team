bd506ca07ed54a3bf5d8ddf337dcaead315b2877
yusshu <yushu@unnamed.team>
2021-12-08 00:47:15 -0500

    fix: fix GitHub repository name pattern

--- pages/docs/[project].tsx
+++ pages/docs/[project].tsx
@@ -1,7 +1,7 @@
 import { NextPageContext } from "next";
 import { DocumentationEntry, DocumentedRepository, fetchRepository } from "../../lib/docs";
 
-const GITHUB_REPO_NAME_PATTERN = /^[A-z0-9\-._]{100}$/g;
+const GITHUB_REPO_NAME_PATTERN = /^[A-z0-9\-._]{1,100}$/g;
 
 export default function Documentation(repository: DocumentedRepository) {
   function convert(entries: Map<string, DocumentationEntry>) {
