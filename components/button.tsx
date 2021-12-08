9351c0c5ae5ac031da5df8b125747e2f28d83bc8
yusshu <yushu@unnamed.team>
2021-12-07 21:41:28 -0500

    lint: fix code indent (2 spaces)

--- components/button.tsx
+++ components/button.tsx
@@ -1,17 +1,18 @@
 type ButtonProps = {
-    style?: 'solid' | 'ghost';
-    children?: any;
+  style?: 'solid' | 'ghost';
+  children?: any;
 };
 
 const styles = {
-    solid: 'bg-pink-light text-white hover:bg-pink-dark',
-    ghost: 'bg-none text-pink-light'
+  solid: 'bg-pink-light text-white hover:bg-pink-dark',
+  ghost: 'bg-none text-pink-light'
 };
 
-const Button = ({ style = 'solid', children }: ButtonProps): JSX.Element => {
-    return <button className={`py-4 px-8 font-normal rounded-full text-lg gap-2 inline-flex flex-row items-center ${styles[style]}`}>
-        {children}
-    </button>;
+const Button = ({style = 'solid', children}: ButtonProps): JSX.Element => {
+  return <button
+    className={`py-4 px-8 font-normal rounded-full text-lg gap-2 inline-flex flex-row items-center ${styles[style]}`}>
+    {children}
+  </button>;
 };
 
 export default Button;
--- pages/404.tsx
+++ pages/404.tsx
@@ -3,30 +3,30 @@ import Head from "next/head";
 import Link from "next/link";
 
 export default function Error404() {
-    return (
-        <>
-            <Head>
-                <title>404 | Not Found</title>
-            </Head>
-            <main className="bg-gradient-to-r from-night-100 to-night-200 h-screen flex flex-row items-center">
-                <div className="w-max mx-auto flex items-center">
-                    <Image src="/logo.svg" width="150" height="100" />
-                    <div className="flex flex-col gap-2 w-96">
-                        <h1 className="font-rubik text-white text-2xl">Error 404</h1>
-                        <p className="font-rubik font-light text-white text-lg">
-                            Sorry, the page you were looking for does not exist. Try
-                            going to
-                            <span className="text-pink-light hover:text-pink-dark">
+  return (
+    <>
+      <Head>
+        <title>404 | Not Found</title>
+      </Head>
+      <main className="bg-gradient-to-r from-night-100 to-night-200 h-screen flex flex-row items-center">
+        <div className="w-max mx-auto flex items-center">
+          <Image src="/logo.svg" width="150" height="100"/>
+          <div className="flex flex-col gap-2 w-96">
+            <h1 className="font-rubik text-white text-2xl">Error 404</h1>
+            <p className="font-rubik font-light text-white text-lg">
+              Sorry, the page you were looking for does not exist. Try
+              going to
+              <span className="text-pink-light hover:text-pink-dark">
                                 <Link href="/"> unnamed.team</Link>
                             </span>
-                            , or join our
-                            <span className="text-pink-light hover:text-pink-dark">
+              , or join our
+              <span className="text-pink-light hover:text-pink-dark">
                                 <Link href="https://discord.gg/xbba2fy"> Discord Server</Link>
                             </span>
-                        </p>
-                    </div>
-                </div>
-            </main>
-        </>
-    );
+            </p>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
