03d06e6cd0bbe301b1a71ab9a64eaa35bc6e603c
yusshu <yushu@unnamed.team>
2021-12-07 22:58:57 -0500

    chore: add '/donate' redirect

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/donate',
+        destination: 'https://paypal.me/yushu7u7',
+        permanent: true,
+        basePath: false
+      },
+    ];
+  }
 };
