2414fa434ec38aa2b99f01b850e867f03b7fce39
yusshu <yushu@unnamed.team>
2021-12-12 23:35:09 -0500

    feat: init glyphs web-editor

---
+++ pages/edit/glyphs.tsx
@@ -0,0 +1,14 @@
+import { Head } from "next/document";
+
+export default function Editor() {
+  return (
+    <>
+      <Head>
+        <meta property="og:title" content="Unnamed | Glyphs"/>
+        <meta property="og:description" content="Web-editor for Glyphs, a Minecraft plugin by Unnamed Team"/>
+        <meta property="theme-color" content="#ff8df8"/>
+        <title>Unnamed | Glyphs</title>
+      </Head>
+    </>
+  );
+}
