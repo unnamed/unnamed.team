bff77e1920fe8e302432835b2b359a5f577b5361
yusshu <yushu@unnamed.team>
2021-12-13 00:44:13 -0500

    chore: add glyph components

--- pages/edit/glyphs.tsx
+++ pages/edit/glyphs.tsx
@@ -1,7 +1,35 @@
 import Head from "next/head";
 import Button from "../../components/button";
+import { CSSProperties, useState } from "react";
+
+interface Glyph {
+  image: string;
+}
+
+function GlyphComponent({glyph}: { glyph: Glyph }) {
+  const imageStyle: CSSProperties = {
+    imageRendering: "pixelated"
+  };
+  return (
+    <div className="flex flex-row bg-pink-light rounded-2xl p-8 w-48">
+      <img src={glyph.image} alt="Glyph" className="w-full" style={imageStyle} />
+    </div>
+  );
+}
 
 export default function Editor() {
+  const [ glyphs, setGlyphs ] = useState(new Map<string, Glyph>());
+
+  function addTestingGlyphs(count: number) {
+    while (count-- > 0) {
+      glyphs.set(count.toString(), {
+        image: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA4AAAAOCAYAAAAfSC3RAAABSklEQVQokZWSwUoCQRjHfzNuhIbaIQ8ZXtSTXroEgQ8QXjx79BJ17RLiA4T0BHnz2BtIz9BdCLTLErEg1K44S7jOdFi1XRGxP8xl5vsN3/zmE7WqxTKG/SIAVpTptxN7Ua3uwgBC1KqW6bcTlAsB7mlvJ5T9vGFkW7S6C+TmwS4omvUD1cwidRRQOL/GG8TuI1PXeAOJmq3Lkc6XDsFAk2sY3p8tcg3DxJVMXBnbU0FY63xpRDkvzeDiFoBkpcTZ1f3WVj9eHvGHYwDqr09/rSYrJfzhmMml3Ar6w/G6BgjlRDeKzYCTrI6tYjOIwWs5fqUDFSgXArw7SaauOTwQAPzMzVJWj5Ft4UetOu6CdCqU8e1JvEEQaVIwsi2OMxo1N0yVjn/HVGneFKRTodFo1Nzg2IboVIpyXgKYfme7lM20HjSAWIHwzyH/BXs5iw9EFkbFAAAAAElFTkSuQmCC"
+      });
+    }
+  }
+
+  addTestingGlyphs(10);
+
   return (
     <>
       <Head>
@@ -11,7 +39,7 @@ export default function Editor() {
         <title>Unnamed | Glyphs</title>
       </Head>
 
-      <div className="flex flex-col bg-gradient-to-r from-night-100 to-night-200 min-h-screen editor px-40 py-10 gap-8">
+      <div className="flex flex-col bg-gradient-to-b from-night-100 to-night-200 min-h-screen editor px-40 py-10 gap-8">
         <div className="font-light">
           <h2 className="text-center text-4xl text-white">Editor</h2>
           <p className="text-center text-lg text-white">Web-editor for µŋglyphs</p>
@@ -29,7 +57,9 @@ export default function Editor() {
           </div>
         </div>
 
-        <div className="emojis flex container">
+        <div className="emojis flex container flex-wrap gap-4 w-max max-w-full self-center">
+          {Array.from(glyphs).map(([ id, glyph ]) =>
+            (<GlyphComponent key={id} glyph={glyph}/>))}
         </div>
       </div>
     </>
