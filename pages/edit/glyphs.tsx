98ad8f78dd3ee6a4a79b9baaafb80dd1ba47dfea
yusshu <yushu@unnamed.team>
2021-12-12 23:51:24 -0500

    chore: add glyphs web-editor body

--- pages/edit/glyphs.tsx
+++ pages/edit/glyphs.tsx
@@ -9,6 +9,24 @@ export default function Editor() {
         <meta property="theme-color" content="#ff8df8"/>
         <title>Unnamed | Glyphs</title>
       </Head>
+
+      <div className="editor px-8 min-h-screen">
+        <h2 className="text-center">Editor</h2>
+        <p className="text-center">Web-editor for µŋglyphs</p>
+
+        <div className="file-input">
+          <h2 className="text-center">Drop your emojis here</h2>
+        </div>
+
+        <div className="flex flex-row">
+          <button>Save</button>
+          <button>Import</button>
+          <button>Export</button>
+        </div>
+
+        <div className="emojis flex container">
+        </div>
+      </div>
     </>
   );
 }
