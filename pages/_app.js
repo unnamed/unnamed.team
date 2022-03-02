afec898a972dede4e940f0ee38c78e041b93e878
yusshu <yushu@unnamed.team>
2022-03-01 22:32:06 -0500

    feat: custom scrollbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import 'tailwindcss/tailwind.css';
 import '../lib/highlight.scss';
+import './styles.scss';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
