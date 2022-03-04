1b0359b73843aa3557948eefb41b8f01f10ca814
yusshu <yushu@unnamed.team>
2022-03-04 02:14:50 -0500

    fix: stop using tailwindcss configuration deprecated fields/values

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  purge: [
+  content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}'
   ],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media',
   theme: {
     colors: {
       white: colors.white,
