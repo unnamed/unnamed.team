8ba1cc6c4aec3ea36315af72e552da34f1d1482d
yusshu <yushu@unnamed.team>
2022-03-05 18:35:09 -0500

    feat: add image-rendering tailwindcss plugin

---
+++ plugins/image-rendering.js
@@ -0,0 +1,22 @@
+/*!
+ * TailwindCSS plugin to add image-rendering
+ * utility classes
+ */
+const plugin = require('tailwindcss/plugin');
+
+module.exports = plugin.withOptions(options => ({ addUtilities }) => {
+  const values = [ 'auto', 'crisp-edges', 'pixelated' ];
+
+  if (options && options.allowExperimentalValues) {
+    values.push('smooth', 'high-quality');
+  }
+
+  const utilities = {};
+  for (const value of values) {
+    utilities[`.rendering-${value}`] = {
+      imageRendering: value
+    };
+  }
+
+  addUtilities(utilities);
+});
