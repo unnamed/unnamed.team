5bc6d5c1b4521029cad8f712557f5c0f29417736
yusshu <yushu@unnamed.team>
2022-03-25 22:28:10 -0500

    fix: center 404 message *absolutely*

--- pages/404.js
+++ pages/404.js
@@ -11,8 +11,8 @@ export default function Error404() {
       <Background>
         <main className="min-h-screen flex flex-col">
           <Header />
-          <div className="flex flex-col flex-1 items-center justify-center">
-            <div className="flex flex-row items-center gap-4">
+          <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+            <div className="flex flex-col items-center gap-4">
               <h1 className="text-lightghost-200 text-3xl">404</h1>
               <p className="text-lightghost-200 font-light text-lg">Sorry, the page you were looking for does not exist</p>
             </div>
