e8db3c89309713d39a27b66d01f4b29f64409aa3
yusshu <yushu@unnamed.team>
2022-04-17 18:30:36 -0500

    chore: remove unused 'Background' component

--- components/Background.js
+++
@@ -1,9 +0,0 @@
-import clsx from 'clsx';
-
-export default function Background({ children, className }) {
-  return (
-    <div className={clsx('min-h-screen bg-wine-900', className)}>
-      {children}
-    </div>
-  );
-}
