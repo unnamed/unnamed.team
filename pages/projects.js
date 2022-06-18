d681af9d6c0f6f73694771aa88020c8a740993ac
yusshu <yushu@unnamed.team>
2022-06-18 16:12:07 -0500

    codestyle: fix codestyle (indent + key)

--- pages/projects.js
+++ pages/projects.js
@@ -6,7 +6,8 @@ import Link from 'next/link';
 function ProjectCard({ project }) {
   return (
     <div className="flex basis-full p-2 md:p-3 md:basis-1/2 xl:basis-1/3">
-      <div className="flex flex-col py-2 md:py-4 px-4 md:px-8 gap-4 w-full justify-between rounded-2xl border bg-white/10 border-white/[.15]">
+      <div
+        className="flex flex-col py-2 md:py-4 px-4 md:px-8 gap-4 w-full justify-between rounded-2xl border bg-white/10 border-white/[.15]">
         <div className="flex flex-col gap-1">
           <p className="font-normal text-white/80">{project.name}</p>
           <p className="font-light text-white/60">{project.description}</p>
@@ -15,9 +16,9 @@ function ProjectCard({ project }) {
           {
             [
               [ `https://github.com/${project.fullName}`, 'GitHub' ],
-              [ `/docs/${project.name}`, 'Docs' ]
+              [ `/docs/${project.name}`, 'Docs' ],
             ].map(([ href, label ]) => (
-              <span className="font-normal text-sm text-white/70 hover:text-pink-200">
+              <span key={href} className="font-normal text-sm text-white/70 hover:text-pink-200">
                 <Link href={href}>{label}</Link>
               </span>
             ))
@@ -36,15 +37,15 @@ export default function Projects({ repos }) {
         <meta property="og:description" content="Our projects: open source libraries and Minecraft plugins"/>
         <meta property="og:url" content="https://unnamed.team/projects"/>
       </Head>
-      <Header />
+      <Header/>
       <div className="container mx-auto px-4 md:px-8">
         <h1 className="p-8 text-white/80 font-medium text-3xl sm:text-4xl md:text-5xl">Our Projects</h1>
         <div className="flex flex-col gap-8 px-8">
           <div className="flex flex-wrap -mx-1">
             {Object.entries(repos)
-                .map(([ key, repo ]) =>
-                  (<ProjectCard key={key} project={repo} />)
-                )}
+              .map(([ key, repo ]) =>
+                (<ProjectCard key={key} project={repo}/>),
+              )}
           </div>
         </div>
       </div>
