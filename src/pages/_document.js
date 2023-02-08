4bc64600e874c75064e70c27985fb003e493b467
yusshu <yushu@unnamed.team>
2023-02-07 23:20:45 -0500

    add some normalization styles, move body background-color to global.scss

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,7 +8,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
         <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet"/>
       </Head>
-      <body className="bg-wine-900">
+      <body>
         <Main />
         <NextScript />
       </body>
