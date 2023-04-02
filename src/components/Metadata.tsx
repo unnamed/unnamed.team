75a325afeb65ce1ff09615940c51f2385c2880bd
yusshu <yushuverduras1309@gmail.com>
2023-04-02 16:24:28 -0500

    feat: add 'description', 'og:type' and 'og:image' metadata

--- src/components/Metadata.tsx
+++ src/components/Metadata.tsx
@@ -11,9 +11,14 @@ export default function Metadata({options}: { options: MetadataOptions }) {
   return (
     <Head>
       <title>{options.title}</title>
+      <meta property="description" content={options.description} />
+
+      {/* OpenGraph Meta */}
+      <meta property="og:type" content="website" />
       <meta property="og:title" content={options.openGraphTitle ?? options.title} />
       <meta property="og:url" content={options.url} />
       <meta property="og:description" content={options.description} />
+      <meta property="og:image" content="/logo_256x256.png" />
     </Head>
   );
 }
