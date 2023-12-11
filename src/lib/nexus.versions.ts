d8339bd8d644358467f88a2bad6f2fb02c236457
yusshu <yushu@unnamed.team>
2023-12-11 00:12:53 -0500

    feat(docs): also check for artifacts in Maven Central

--- src/lib/nexus.versions.ts
+++ src/lib/nexus.versions.ts
@@ -17,6 +17,22 @@ export async function fetchVersioning(groupId: string, artifactId: string): Prom
     return versions;
   }
 
+  {
+    // try looking in Maven Central first
+    const query = `g:${groupId}+AND+a:${artifactId}`;
+    const url = `https://search.maven.org/solrsearch/select?q=${query}&rows=1&wt=json`;
+    const response = (await (await fetch(url)).json()).response;
+    if (response.numFound > 0) {
+      const doc = response.docs[0];
+      versions = {
+        latest: doc.latestVersion,
+        release: doc.latestVersion
+      };
+      versionCache.set(key, versions);
+      return versions;
+    }
+  }
+
   const location = `${groupId.replace(/\./g, '/')}/${artifactId}`;
   const url = `${BASE_URL}/repository/${DEFAULT_REPOSITORY}/${location}/maven-metadata.xml`;
 
